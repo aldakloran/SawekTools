be73337e9a9774f681a45697898f78c6e771d886
Michał Smoliński <aldakloran@gmail.com>
2019-09-03 12:19:33 +0200

    add SawekObjectExtensions

---
+++ SawekTools/SawekObjectExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace SawekTools {
+    public static class SawekObjectExtensions {
+        public static ICollection<string> CopyObjectToObject<T>(this T TargetObject, T SourceObject) where T : class {
+            var diffList = new List<string>();
+
+            foreach (var field in typeof(T).GetProperties()) {
+                if (!field.CanWrite) continue;
+
+                var targetValue = field.GetValue(TargetObject);
+                var sourceValue = field.GetValue(SourceObject);
+
+                if (targetValue == null && sourceValue == null) continue;
+                if (targetValue != null && targetValue.Equals(sourceValue)) continue;
+
+                field.SetValue(TargetObject, sourceValue);
+                diffList.Add(field.Name);
+            }
+
+            return diffList;
+        }
+    }
+}
